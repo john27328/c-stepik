cc019da5d5beb4ff5dfd74c3fefa87a910ae5ca0
john27328 <john27328@gmail.com>
2019-03-08 20:23:54 +0300

    Квадратное уравнение

---
+++ Quadratic/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    int d = b * b - 4 * a * c;
+    if (d < 0)
+        cout << "No real roots" << endl;
+    else {
+        cout << (- b + sqrt(d)) / (2 * a) << (- b - sqrt(d)) / (2 * a) << endl;
+    }
+    return 0;
+}
