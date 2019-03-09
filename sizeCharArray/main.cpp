e46db1f3588395e97f91c8ae71d322392f935b59
john27328 <john27328@gmail.com>
2019-03-09 19:02:46 +0300

    размер строки

---
+++ sizeCharArray/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+unsigned strlen(const char *str)
+{
+    char *p = (char *)str;
+    while( *p != '\0')
+    {
+        p++;
+    }
+    return p-str;
+}
+
+
+int main()
+{
+    char str[] = "C-style";
+    cout << strlen(str);
+    return 0;
+}
+
