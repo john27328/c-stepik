5284e74adcacffff54f3de8bc8219f17dcddac99
john27328 <john27328@gmail.com>
2019-03-09 17:10:00 +0300

    наибольший общий делитель

--- GCD/main.cpp
+++ GCD/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 
+unsigned gcd(unsigned a, unsigned b);
+
 using namespace std;
 
 int main()
 {
     cout << "Hello World!" << endl;
+    unsigned a = 6, b = 9;
+    cout << gcd(a, b) <<endl;
     return 0;
 }
+
+unsigned gcd(unsigned a, unsigned b)
+{
+    if (a > b){
+        unsigned tmp = a;
+        a = b;
+        b = tmp;
+    }
+    if (a == 0) {
+        return b;
+    }
+    else {
+        gcd(a, b % a);
+    }
+}
+
