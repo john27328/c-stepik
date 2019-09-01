810716a5161209096a72a6eab9e04cab370c473d
Ivan Raptev <john27328@gmail.com>
2019-09-01 18:43:54 +0300

    printf

--- print_values/main.cpp
+++ print_values/main.cpp
@@ -18,6 +18,32 @@ void print_values(std::ostream & os, T value, Args... args)
     print_values(os, args...);
 }
 
+// принимает произвольное число аргументов
+void printf(std::ostream & os, char const *s)
+{
+    while (*s){
+        if(*s == '%' && *(++s) != '%')
+            std::cout << "error" << std::endl;
+        os << *s++;
+    }
+}
+
+template<typename T, typename... Args>
+void printf(std::ostream & os, char const *s, T value, Args... args)
+{
+    os << typeid(value).name() << ": " << value <<std::endl;
+    while (*s){
+        if(*s == '%' && *(++s) != '%') {
+            os << value;
+            printf(os, args...);
+            return;
+        }
+        os << *s++;
+    }
+    std::cout << "error" << std::endl;
+}
+
+
 int main()
 {
     print_values(std::cout, 0, 3.5, "Hello");
