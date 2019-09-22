f9755c41de31d01d35bdb2106d5018f63d0eb844
Ivan Raptev <john27328@gmail.com>
2019-09-22 19:12:12 +0300

    end

--- Multimethod2/main.cpp
+++ Multimethod2/main.cpp
@@ -4,6 +4,7 @@
 #include <typeindex>
 #include <map>
 #include <tuple>
+#include <functional>
 
 
 // базовый класс фигуры (полиморфный)
@@ -27,19 +28,19 @@ struct Triangle : Shape {
 };
 
 // функция для проверки пересечения двух прямоугольников
-bool is_intersect_r_r(Shape * a, Shape * b){
+void is_intersect_r_r(Shape * a, Shape * b){
     std::type_index tA = typeid (*a);
     std::type_index tB = typeid (*b);
     std::cout << "is_intersect_r_r " << tA.name() << " " << tB.name() << std::endl;
-    return true;
+//    return true;
 }
 
 // функция для проверки пересечения прямоугольника и треугольника
-bool is_intersect_r_t(Shape * a, Shape * b){
+void is_intersect_r_t(Shape * a, Shape * b){
     std::type_index tA = typeid (*a);
     std::type_index tB = typeid (*b);
     std::cout << "is_intersect_r_t  " << tA.name() << " " << tB.name() << std::endl;
-    return true;
+//    return true;
 }
 
 
@@ -53,8 +54,8 @@ template<class Base, class Result, bool Commutative>
 struct Multimethod2
 {
     using type = const std::type_index;
-    using doubleType = std::tuple<type, type>;
-    using pF = Result (*)(Shape*, Shape*);
+    using doubleType = std::pair<type, type>;
+    using pF = std::function<Result(Base *, Base*)>;
 private:
     std::map<doubleType, pF> _map;
 public:
@@ -64,10 +65,10 @@ public:
     // принимающий два указателя на Base
     // и возвращающий значение типа Result
     // добавляет реализацию мультиметода для двух типов, которые заданы через std::type_info.
-    void addImpl(type a, type b, pF f)
+    void addImpl(const type &a, const type &b, pF f)
     {
 //        std::cout << "addImpl " << a.name() << " " << b.name() << std::endl;
-        _map.emplace(std::make_tuple(a, b), f);
+        _map.emplace(std::make_pair(a, b), f);
     }
 
     // проверяет, есть ли реализация мультиметода
@@ -78,13 +79,13 @@ public:
         std::type_index tA = typeid (*a);
         std::type_index tB = typeid (*b);
 //        std::cout << "hasImpl " << tA.name() << " " << tB.name() << std::endl;
-        auto it = _map.find(std::make_tuple(tA, tB));
+        auto it = _map.find(std::make_pair(tA, tB));
         if (it  != _map.end()){
 //            std::cout << "hasImpl true" << std::endl;
             return true;
         }
         if (Commutative){
-            it = _map.find(std::make_tuple(tB, tA));
+            it = _map.find(std::make_pair(tB, tA));
             if (it  != _map.end()){
     //            std::cout << "hasImpl true" << std::endl;
                 return true;
@@ -101,20 +102,19 @@ public:
     {
         std::type_index tA = typeid (*a);
         std::type_index tB = typeid (*b);
-        auto it = _map.find(std::make_tuple(tA, tB));
+        auto it = _map.find(std::make_pair(tA, tB));
         if (it != _map.end()){
             //pF f = it->second;
             //Result tmp = (f)(a,b);
             return (it->second)(a,b);
         }
         if (Commutative){
-            it = _map.find(std::make_tuple(tB, tA));
+            it = _map.find(std::make_pair(tB, tA));
             if (it  != _map.end()){
     //            std::cout << "hasImpl true" << std::endl;
                 return (it->second)(b,a);
             }
         }
-        return 0;
     }
 
 };
@@ -123,7 +123,8 @@ int main()
 {
     // мультиметод для наследников Shape
     // возращающий bool и являющийся коммутативным
-    Multimethod2<Shape, bool, true> is_intersect;
+    Multimethod2<Shape, void, true> is_intersect;
+    Multimethod2<std::string, std::string, true> is_intersect2;
 
     // добавляем реализацию мультиметода для двух прямоугольников
     is_intersect.addImpl(typeid(Rectangle), typeid(Rectangle), is_intersect_r_r);
@@ -136,10 +137,10 @@ int main()
     Shape * s2 = new Rectangle();
 
     // проверяем, что реализация для s1 и s2 есть
-    if (is_intersect.hasImpl(s1, s2))
+    if (is_intersect.hasImpl(s2, s2))
     {
          // вызывается функция is_intersect_r_t(s2, s1)
-         bool res = is_intersect.call(s1, s2);
+         is_intersect.call(s2, s2);
 
          // Замечание: is_intersect_r_t ожидает,
          // что первым аргументом будет прямоугольник
