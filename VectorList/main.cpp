1950556c21622c2e0d33b0ebc3d68e4c68553e59
john <john27328@gmail.com>
2019-10-19 18:51:39 +0300

    наконецто!!!!!

--- VectorList/main.cpp
+++ VectorList/main.cpp
@@ -36,7 +36,9 @@ public:
     {
         size_t s = 0;
         for(auto &i: data_){
-            s +=  i.size();
+            for(auto &j: i){
+            s++;
+            }
         }
         return s;
     }
@@ -63,52 +65,48 @@ public:
 
         const_iterator &operator++ (){
             itVector_++;
-            if(itVector_ == (*itList_).end()){
+            if (itVector_ == itList_->end()){
                 itList_++;
-                if(itList_ != (*p_).end())
+                if(itList_ != p_->end())
                 {
-                    itVector_ = (*itList_).begin();
+                    itVector_ = itList_->begin();
                 }
+                else
+                    itList_--;
             }
             return *this;
         }
 
         const_iterator operator++(int){
-            auto tmp = *this;
+            auto tmp(*this);
                 ++*this;
             return tmp;
         }
 
         const_iterator &operator-- (){
-            if(itList_ == (*p_).end())
+            if(itVector_ == itList_->begin()){
                 --itList_;
-            if(itVector_ == (*itList_).begin()){
-                --itList_;
-                itVector_ = --((*itList_).end());
-            } else {
-                --itVector_;
+                itVector_ = itList_->end();
             }
+            --itVector_;
             return *this;
         }
 
         const_iterator operator--(int){
-            auto tmp = *this;
+            auto tmp(*this);
                 --*this;
             return tmp;
         }
 
-        bool operator== (const const_iterator  it){
+        bool operator== (const const_iterator  it) const{
             if(itList_ == it.itList_ && itVector_ == it.itVector_)
                 return 1;
             else
                 return 0;
         }
 
-        bool operator!= (const const_iterator  it){
-            if(itList_ != it.itList_ || itVector_ != it.itVector_)
-                return 1;
-            else
-                return 0;
+        bool operator!= (const const_iterator  it) const{
+            return !(*this == it);
         }
 
     private:
@@ -119,13 +117,17 @@ public:
 
     // определите методы begin / end
     const_iterator begin() const {
-         return const_iterator(&data_, data_.begin(), (*(data_.begin())).begin());
+         return const_iterator(&data_, data_.begin(), data_.begin()->begin());
     }
     const_iterator end()   const {
-        return const_iterator(&data_, data_.end(), (*(--(data_.end()))).end()); }
+        if (data_.empty())
+            return const_iterator(&data_, data_.begin(), data_.begin()->begin());
+        else
+            return const_iterator(&data_, --data_.end(), (--data_.end())->end());
+    }
 
     // определите const_reverse_iterator
-    using const_reverse_iterator = const std::reverse_iterator<const_iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
     // определите методы rbegin / rend
     const_reverse_iterator rbegin() const {
@@ -141,57 +143,83 @@ private:
 
 int main()
 {
-    VectorList<char> v;
-    auto *p = &v;
-    int q = 0;
-    for(int i = 0; i < 10; i++){
-        std::vector<char> vec;
-        for (int j = 0; j < 10; j++){
-            vec.push_back(q++);
-            std::cout << q - 1 <<" ";
-        }
-        v.append(vec.begin(), vec.end());
-        std::cout << std::endl;
-    }
+  VectorList<char> vlist;
+  std::cout << "empty size is " << vlist.size() << " " << vlist.empty() << std::endl;
+  auto q1 = vlist.begin();
+  auto q2 = vlist.end();
 
-    //list<vector<double>>::iterator it;
-    std::cout << v.size() << std::endl;
-    auto it = v.begin();
-    std::cout << *++it << std::endl;
-    std::cout << *it-- << std::endl;
-    std::cout << *it << std::endl;
-    std::cout << *--(v.end()) << std::endl;
+  std::cout << "empty vlist.begin() == vlist.end() " << (q1 == q2) << std::endl;
 
-    for(auto i: v){
-        std::cout << i << " ";
-    }
-    std::cout << std::endl;
+  std::cout << "std::distance(begin,end)﻿ " << (std::distance(vlist.begin(), vlist.end())) << std::endl;
+  typename VectorList<char>::const_iterator it1 = vlist.begin();
+  typename VectorList<char>::const_reverse_iterator rit(it1);
+  typename VectorList<char>::const_iterator it2 = rit.base();
+  if (it1 == it2)
+      std::cout << "base() works fine" << std::endl;
+  else
+      std::cout << "base() doesn't work as expected" << std::endl;
 
-    auto __tmp = it;
-    std::cout << *++__tmp << std::endl;
+  std::vector<char> v1;
+  v1.push_back('A');
+  v1.push_back('B');
+  v1.push_back('C');
 
-    auto rit = v.rbegin();
-    auto rend = v.rend();
-    std::cout << *--rend<< std::endl;
-    for(; rit != v.rend(); ++rit){
-        std::cout << *rit << " ";
-    }
-    std::cout << std::endl;
+  std::vector<char> v2;
+  v2.push_back('D');
+  v2.push_back('E');
+  v2.push_back('F');
+  v2.push_back('G');
+  vlist.append(v1.begin(), v1.end());
+  vlist.append(v2.begin(), v2.end());
 
-    VectorList<std::vector<double>> v2;
-    for(int i = 0; i < 10; i++){
-        std::vector<std::vector<double>> vec;
-        std::vector<double> tmp;
-        for (int j = 0; j < 10; j++){
-            tmp.push_back(10+i+j);
-            tmp.push_back(10+j-i);
-            vec.push_back(tmp);
-        }
-        v2.append(vec.begin(), vec.end());
-        //std::cout << std::endl;
-    }
-    auto it2 = v2.begin();
-    std::cout << (++++++++++it2)->at(1)<< std::endl;
+  auto i = vlist.begin();
 
-    return 0;
+  std::cout << "Size is " << vlist.size() << " " << vlist.empty() << std::endl;
+  std::cout << "begin is " << *i << std::endl;
+  std::cout << "std::distance(begin,end)﻿ " << (std::distance(vlist.begin(), vlist.end())) << std::endl;
+  std::cout << "*(++begin) == 'B'? " << (*++vlist.begin() == 'B') << std::endl;
+  std::cout << "*(++begin) == 'A'? " << (*++vlist.begin() == 'A') << std::endl;
+  std::cout << std::endl;
+
+
+  std::cout << "Test ++i" << std::endl;
+  for (i = vlist.begin(); i != vlist.end(); ++i)
+    std::cout << *i << " ";
+  std::cout << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "Test i++" << std::endl;
+  for (i = vlist.begin(); i != vlist.end(); i++)
+    std::cout << *i << " ";
+  std::cout << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "Test --i" << std::endl;
+  for (i = vlist.end(); i != vlist.begin();)
+    std::cout << *--i << " ";
+  std::cout << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "Test i--" << std::endl;
+  for (i = vlist.end(); i != vlist.begin();) {
+    i--;
+    std::cout << *i << " ";
+  }
+  std::cout << std::endl;
+  std::cout << std::endl;
+
+
+  std::cout << std::endl;
+  auto j = vlist.rbegin(); j++;
+  std::cout << "rbegin is " << *j << std::endl;
+  j = --vlist.rend();
+  std::cout << "--rend is " << *--j << std::endl;
+
+  std::cout << "Test reverse_const_iterator ++" << std::endl;
+  for (j = vlist.rbegin(); j != vlist.rend(); ++j)
+    std::cout << *j << " ";
+  std::cout << std::endl;
+
+  //system("pause");
+  return 0;
 }
