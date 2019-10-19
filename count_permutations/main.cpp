6313e46e43b6c65c30ee1f6191082b00c3597f48
john <john27328@gmail.com>
2019-10-19 21:51:58 +0300

    решил

--- count_permutations/main.cpp
+++ count_permutations/main.cpp
@@ -5,19 +5,21 @@
 template<class Iterator>
 size_t count_permutations(Iterator p, Iterator q)
 {
+    if (p == q) return 1;
     std::vector<Iterator> vec;
     for(auto i = p; i != q; i++){
         vec.push_back(i);
     }
     auto less = [](Iterator i1, Iterator i2){return (*i1) < (*i2);};
     std::sort(vec.begin(), vec.end(), less);
-    size_t s = 1;
-    while(std::next_permutation(vec.begin(), vec.end(), less)){
+    size_t s = 0;
+    auto adj = std::adjacent_find(vec.begin(), vec.end(), [](Iterator i1, Iterator i2){return (*i1) == (*i2);});
+    if(adj == vec.end())
         s++;
-        for(auto i: vec){
-            std::cout << *i << " ";
-        }
-        std::cout << std::endl;
+    while(std::next_permutation(vec.begin(), vec.end(), less)){
+        auto adj = std::adjacent_find(vec.begin(), vec.end(), [](Iterator i1, Iterator i2){return (*i1) == (*i2);});
+        if(adj == vec.end())
+            s++;
     }
     return s;
 }
@@ -26,7 +28,7 @@ using namespace std;
 
 int main()
 {
-    vector<int> vec = {4, 3, 2};
+    vector<int> vec = {3, 1, 1};
     cout << count_permutations(vec.begin(), vec.end()) << " " << endl;
     return 0;
 }
