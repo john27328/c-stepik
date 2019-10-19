6329119cdd0d32d5f26559dfc30cbed95132ac54
john <john27328@gmail.com>
2019-10-19 20:09:32 +0300

    мой велосипед

---
+++ remove_nth/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// алгоритм должен работать с forward итераторами
+// и возвращать итератор, который потом будет передан
+// в метод erase соответствующего контейнера
+template<class FwdIt>
+FwdIt remove_nth(FwdIt p, FwdIt q, size_t n)
+{
+    auto tmp = p;
+    for(size_t i = 0; p!=q; ++p, ++i){
+        (*tmp) = (*p);
+        if (i != n){
+            ++tmp;
+        }
+    }
+    return tmp;
+}
+
+int main()
+{
+    std::vector<int> v = {0,1,2,3,4,5,6,7,8,9,10};
+    v.erase(remove_nth(v.begin(), v.end(), 5), v.end());
+    for(auto &i: v){
+        std::cout << i << ", ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
