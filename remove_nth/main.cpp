134a70c32bb890c92cb1a7b176862bb7257c41ea
john <john27328@gmail.com>
2019-10-19 20:10:40 +0300

    чужое решение

--- remove_nth/main.cpp
+++ remove_nth/main.cpp
@@ -8,14 +8,7 @@
 template<class FwdIt>
 FwdIt remove_nth(FwdIt p, FwdIt q, size_t n)
 {
-    auto tmp = p;
-    for(size_t i = 0; p!=q; ++p, ++i){
-        (*tmp) = (*p);
-        if (i != n){
-            ++tmp;
-        }
-    }
-    return tmp;
+    return std::remove_if(p, q, [&n](decltype(*p) val) { return n-- == 0; });
 }
 
 int main()
