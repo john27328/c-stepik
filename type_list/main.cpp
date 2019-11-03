f73aea5e5af56776ac28ccae761b8f3621e1a9e9
john <john27328@gmail.com>
2019-11-03 15:25:06 +0300

    upgrade

--- type_list/main.cpp
+++ type_list/main.cpp
@@ -12,10 +12,23 @@ struct IntList<I, Ints...>{
 template <>
 struct IntList<>{};
 
+template <typename IL>
+struct Length{
+    static int const value = 1 + Length<typename IL::Tail>::value;
+};
+
+template <>
+struct Length<IntList<>>{
+    static int const value = 0;
+};
+
 int main()
 {
     using primes = IntList<2,3,5,7,11,13>;
     constexpr int head = primes::Head;
     using odd_primes = primes::Tail;
+
+    constexpr size_t len = Length<primes>::value; // 6
+    std::cout << len << std::endl;
     return 0;
 }
